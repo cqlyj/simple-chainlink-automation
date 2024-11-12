6e8e01597894b262a2ac2e68ce82fcd7ff968df8
cqlyj <luoyingjie0721@gmail.com>
2024-11-12 22:49:52 +0800

    feat: add PostNft contract

---
+++ src/PostNft.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PostNft is ERC721URIStorage, Ownable {
+    uint256 private counter;
+
+    constructor() ERC721("PostNft", "PN") Ownable(msg.sender) {
+        counter = 0;
+    }
+
+    function mint(address to, string memory tokenURI) public onlyOwner {
+        _safeMint(to, counter);
+        _setTokenURI(counter, tokenURI); // Store metadata URL
+        counter++;
+    }
+}
