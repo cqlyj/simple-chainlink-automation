813ee1e0ad3c419110fcf28c6dcd71898821a22e
cqlyj <luoyingjie0721@gmail.com>
2024-11-13 00:09:24 +0800

    feat: remove Ownable inheritance from PostNft contract and update mint function visibility

--- src/PostNft.sol
+++ src/PostNft.sol
@@ -2,16 +2,15 @@
 pragma solidity 0.8.25;
 
 import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PostNft is ERC721URIStorage, Ownable {
+contract PostNft is ERC721URIStorage {
     uint256 private counter;
 
-    constructor() ERC721("PostNft", "PN") Ownable(msg.sender) {
+    constructor() ERC721("PostNft", "PN") {
         counter = 0;
     }
 
-    function mint(address to, string memory tokenURI) external onlyOwner {
+    function mint(address to, string memory tokenURI) external {
         _safeMint(to, counter);
         _setTokenURI(counter, tokenURI); // Store metadata URL
         counter++;
